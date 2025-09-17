7ea37f7df4edf75a3c6762224206510dfb82b693
JackDanna <jackdanna78@gmail.com>
2025-09-17 07:06:35 -0400

    removed old thing

--- flake.nix
+++ flake.nix
@@ -23,12 +23,6 @@
     };
   in
   {
-    packages.${system} = {
-      default = pkgs.writeShellScriptBin "run" ''
-        nix develop -c -- codium .
-      '';
-    };
-
     devShells.${system}.default = pkgs.mkShell rec {
       name = "CDevShell";
       buildInputs = with pkgs; [
